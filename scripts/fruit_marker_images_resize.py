6cd2e24b15ee4138bd13e651132ac69211808d60
Jan Konopka <janfrederik.konopka@gmail.com>
2021-04-07 20:44:49 +0200

    change: use xxxhdpi instead of xhdpi fruit icons

--- scripts/fruit_marker_images_resize.py
+++ scripts/fruit_marker_images_resize.py
@@ -5,16 +5,33 @@ import shutil
 import PIL.Image
 import PIL.ImageOps
 
-shutil.rmtree('fruit_markers_resized')
-os.mkdir('fruit_markers_resized')
+__dir__ = os.path.dirname(os.path.abspath(__file__))
+src_dir = os.path.dirname(__dir__) + '/images/fruit_markers_vector_2021'
+os.makedirs(src_dir.replace('2021', '2021_resized'), exist_ok=True)
+print('src_dir=', src_dir)
 
-for fn in glob.glob('fruit_markers_drawable/*.png'):
+
+# *** make resized images ***
+for fn in glob.glob(src_dir + '/*.png'):
     image = PIL.Image.open(fn).convert('RGBA')
 
     # add 1px border so antialiasing looks good near the border
     image = PIL.ImageOps.expand(image, border=1)
-    image.thumbnail((40, 40), PIL.Image.ANTIALIAS)
+    image = image.resize((100, 160), PIL.Image.ANTIALIAS)
 
-    fn_new = fn.replace('drawable', 'resized')
+    fn_new = fn.replace('2021', '2021_resized')
     print(fn, '->', fn_new)
     image.save(fn_new)
+
+
+# # *** make photo placeholders ***
+# for fn in glob.glob(src_dir + '/*.png'):
+#     image = PIL.Image.open(fn).convert('RGBA')
+
+#     # add 1px border so antialiasing looks good near the border
+#     image = PIL.ImageOps.expand(image, border=1)
+#     image = image.resize((100, 160), PIL.Image.ANTIALIAS)
+
+#     fn_new = fn.replace('2021', '2021_resized')
+#     print(fn, '->', fn_new)
+#     image.save(fn_new)
