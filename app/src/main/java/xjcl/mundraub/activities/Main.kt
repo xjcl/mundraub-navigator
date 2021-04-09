39f3b8b92e08f3b7be45548fcfea11e8a424d489
Jan Konopka <janfrederik.konopka@gmail.com>
2021-04-09 13:31:25 +0200

    refactor: move ItemMenu.kt to own class

--- app/src/main/java/xjcl/mundraub/activities/Main.kt
+++ app/src/main/java/xjcl/mundraub/activities/Main.kt
@@ -66,18 +66,6 @@ class Main : AppCompatActivity(), OnMapReadyCallback, OnCameraIdleListener, Acti
     private val polylinesOnScreen = mutableListOf<Polyline>()
     private var polylinesLatLng : LatLng? = null
 
-    enum class ItemMenu(val value: Int) {
-        PLANT_LIST(1),
-        PLANT_FORM(2),
-        PLANT_ATLAS(10),
-        LOGOUT(68),
-        SETTINGS(69),
-        MUNDRAUB_RULES(82),
-        FLORA_INCOGNITA(83),
-        PRIVACY(98),
-        IMPRINT(99),
-    }
-
     private fun removePolylines() {
         if (polylinesOnScreen.isEmpty()) return
         runOnUiThread {
---
+++ app/src/main/java/xjcl/mundraub/data/ItemMenu.kt
@@ -0,0 +1,13 @@
+package xjcl.mundraub.data
+
+enum class ItemMenu(val value: Int) {
+    PLANT_LIST(1),
+    PLANT_FORM(2),
+    PLANT_ATLAS(10),
+    LOGOUT(68),
+    SETTINGS(69),
+    MUNDRAUB_RULES(82),
+    FLORA_INCOGNITA(83),
+    PRIVACY(98),
+    IMPRINT(99),
+}
